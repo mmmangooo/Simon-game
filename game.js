22ef048da4b80d379324e4205dde991a4bb9516d
mmmangooo <isabelle.angsas@gmail.com>
2021-08-20 16:26:09 +0200

    Initial commit

---
+++ game.js
@@ -0,0 +1,23 @@
+var buttonColours = ["red", "blue", "green", "yellow"];
+var gamePattern = [];
+
+function nextSequence() {
+    // Generate random number,
+    var randomNumber = Math.floor(Math.random() * 4);
+    // using random number to generate a random colour
+    var randomChosenColour = buttonColours[randomNumber];
+    // Push generated color to array
+    gamePattern.push(randomChosenColour);
+    
+    // Setting a flash animation to the button with the generated colour
+    setInterval(function() {$("#" +randomChosenColour).fadeOut(100).fadeIn(100); }
+, 900);
+
+// Creating an Audio object with its url set to the generated color's sound
+let buttonAudio = new Audio("sounds/" +randomChosenColour +".mp3");
+// Playing the sound file
+buttonAudio.play();
+}
+
+
+
