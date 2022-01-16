f21f9bc4f0ba63f321b121bd4eaf0885784fbb97
Isabelle Ängsås <64039004+mmmangooo@users.noreply.github.com>
2022-01-16 21:02:24 +0100

    Added logic for starting the game and storing user clicked colors

--- game.js
+++ game.js
@@ -1,32 +1,58 @@
-var buttonColours = ["red", "blue", "green", "yellow"];
-var gamePattern = [];
+let buttonColours = ["red", "blue", "green", "yellow"];
+let gamePattern = [];
+let userClickedPattern = [];
 
 function nextSequence() {
     // Generate random number,
-    var randomNumber = Math.floor(Math.random() * 4);
+    let randomNumber = Math.floor(Math.random() * 4);
     // using random number to generate a random colour
-    var randomChosenColour = buttonColours[randomNumber];
+    let randomChosenColour = buttonColours[randomNumber];
     // Push generated color to array
     gamePattern.push(randomChosenColour);
-    
-    // Setting a flash animation to the button with the generated colour
-    setInterval(function() {$("#" +randomChosenColour).fadeOut(100).fadeIn(100); }
-, 900);
 
-// Creating an Audio object with its url set to the generated color's sound
-let buttonAudio = new Audio("sounds/" +randomChosenColour +".mp3");
-// Playing the sound file
-buttonAudio.play();
+    // Setting a flash animation to the button with the generated colour
+    setInterval(function() { $("#" + randomChosenColour).fadeOut(100).fadeIn(100); }, 900);
+
+    // Creating an Audio object with its url set to the generated color's sound
+    let buttonAudio = new Audio("sounds/" + randomChosenColour + ".mp3");
+    // Playing the sound file
+    buttonAudio.play();
 }
+
+
+// Assigning a nodelist of all buttons to a variable,
+let buttons = document.querySelectorAll(".btn");
+
+// looping over the nodelist and for each element in it 
+// adding an event listener that triggers a handler 
+// function when the element is clicked.
+buttons.forEach(el => el.addEventListener("click", event => {
+    let userChosenColor = el.id;
+
+    //Pushing the color of the button clicked to an array
+    // storing the pattern of user clicked colors.
+    userClickedPattern.push(userChosenColor);
+
+}));
+
+
+
 
 let gameStarted = false;
 let level = 0;
 
-document.onkeypress = function() {
+
+// Starting the game when the user presses a key,
+// if the game is not already started. Chaning the 
+// header text to "Game started"
+document.onkeydown = function() {
     if (gameStarted == false) {
-    nextSequence();
-    gameStarted = true;
-    document.getElementsById("level-title").innerHTML = "Game Started";
+        nextSequence();
+        gameStarted = true;
+        document.getElementById("level-title").innerHTML = "Game Started";
     };
 };
 
+function checkAnswer(currentLevel) {
+
+}
