2b6608b431b6c2727887c4c643dd97d88205efac
Isabelle Ängsås <64039004+mmmangooo@users.noreply.github.com>
2022-04-05 19:52:09 +0200

    Added functionality for checking user's answer against generated sequence

--- game.js
+++ game.js
@@ -1,23 +1,9 @@
 let buttonColours = ["red", "blue", "green", "yellow"];
 let gamePattern = [];
 let userClickedPattern = [];
-
-function nextSequence() {
-    // Generate random number,
-    let randomNumber = Math.floor(Math.random() * 4);
-    // using random number to generate a random colour
-    let randomChosenColour = buttonColours[randomNumber];
-    // Push generated color to array
-    gamePattern.push(randomChosenColour);
-
-    // Setting a flash animation to the button with the generated colour
-    setInterval(function() { $("#" + randomChosenColour).fadeOut(100).fadeIn(100); }, 900);
-
-    // Creating an Audio object with its url set to the generated color's sound
-    let buttonAudio = new Audio("sounds/" + randomChosenColour + ".mp3");
-    // Playing the sound file
-    buttonAudio.play();
-}
+let gameStarted = false;
+let level = 0;
+let lastUserClicked = userClickedPattern[userClickedPattern.length - 1];
 
 
 // Assigning a nodelist of all buttons to a variable,
@@ -32,27 +18,53 @@ buttons.forEach(el => el.addEventListener("click", event => {
     //Pushing the color of the button clicked to an array
     // storing the pattern of user clicked colors.
     userClickedPattern.push(userChosenColor);
-
+    checkAnswer(lastUserClicked);
 }));
 
 
 
-
-let gameStarted = false;
-let level = 0;
-
-
 // Starting the game when the user presses a key,
-// if the game is not already started. Chaning the 
+// if the game is not already started. Changing the 
 // header text to "Game started"
 document.onkeydown = function() {
     if (gameStarted == false) {
-        nextSequence();
         gameStarted = true;
         document.getElementById("level-title").innerHTML = "Game Started";
+        setTimeout(function() { nextSequence() }, 2000);
     };
 };
 
-function checkAnswer(currentLevel) {
+function nextSequence() {
 
+    userClickedPattern = [];
+    level++;
+    document.getElementById("level-title").innerHTML = "Level " + level;
+    // Generate random number,
+    let randomNumber = Math.floor(Math.random() * 4);
+    // using random number to generate a random colour
+    let randomChosenColour = buttonColours[randomNumber];
+    // Push generated color to array
+    gamePattern.push(randomChosenColour);
+
+    // Setting a flash animation to the button with the generated colour
+    setInterval(function() { $("#" + randomChosenColour).fadeOut(100).fadeIn(100); }, 900);
+
+    // Creating an Audio object with its url set to the generated color's sound
+    let buttonAudio = new Audio("sounds/" + randomChosenColour + ".mp3");
+    // Playing the sound file
+    buttonAudio.play();
+
+}
+
+
+// Checking if the buttons clicked by the user are the same as the ones listed in 
+// the game pattern, and if so triggering the nextSequense function when the user
+// has finished the entire sequence
+function checkAnswer(currentLevel) {
+    if (userClickedPattern[currentLevel] == gamePattern[currentLevel]) {
+        if (gamePattern.length == userClickedPattern.length) {
+            setTimeout(function() { nextSequence() }, 1000);
+
+        }
+    }
 }
