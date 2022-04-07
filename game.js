b6ea4af1d190ca00614da9eb53a72b5e667129a8
Isabelle Ängsås <64039004+mmmangooo@users.noreply.github.com>
2022-04-07 20:35:57 +0200

    Finished debugging checkAnswer() and added game over functionality

--- game.js
+++ game.js
@@ -3,8 +3,20 @@ let gamePattern = [];
 let userClickedPattern = [];
 let gameStarted = false;
 let level = 0;
-let lastUserClicked = userClickedPattern[userClickedPattern.length - 1];
 
+
+// Starting the game when the user presses a key,
+// if the game is not already started. Changing the 
+// header text to "Game started"
+document.onkeydown = function() {
+    if (gameStarted == false) {
+
+        document.getElementById("level-title").innerHTML = "Game Started";
+        setTimeout(function() { nextSequence() }, 2000);
+
+        gameStarted = true;
+    };
+};
 
 // Assigning a nodelist of all buttons to a variable,
 let buttons = document.querySelectorAll(".btn");
@@ -15,30 +27,26 @@ let buttons = document.querySelectorAll(".btn");
 buttons.forEach(el => el.addEventListener("click", event => {
     let userChosenColor = el.id;
 
+    animatePress(userChosenColor);
+    playSound(userChosenColor);
+
     //Pushing the color of the button clicked to an array
     // storing the pattern of user clicked colors.
     userClickedPattern.push(userChosenColor);
-    checkAnswer(lastUserClicked);
+
+    //Calling checkAnswer to see if the user clicked the right button
+    checkAnswer(userClickedPattern.length - 1);
 }));
 
 
-
-// Starting the game when the user presses a key,
-// if the game is not already started. Changing the 
-// header text to "Game started"
-document.onkeydown = function() {
-    if (gameStarted == false) {
-        gameStarted = true;
-        document.getElementById("level-title").innerHTML = "Game Started";
-        setTimeout(function() { nextSequence() }, 2000);
-    };
-};
-
 function nextSequence() {
 
+    //Resetting userClickedPattern to an empty array
     userClickedPattern = [];
+
     level++;
     document.getElementById("level-title").innerHTML = "Level " + level;
+
     // Generate random number,
     let randomNumber = Math.floor(Math.random() * 4);
     // using random number to generate a random colour
@@ -46,13 +54,8 @@ function nextSequence() {
     // Push generated color to array
     gamePattern.push(randomChosenColour);
 
-    // Setting a flash animation to the button with the generated colour
-    setInterval(function() { $("#" + randomChosenColour).fadeOut(100).fadeIn(100); }, 900);
-
-    // Creating an Audio object with its url set to the generated color's sound
-    let buttonAudio = new Audio("sounds/" + randomChosenColour + ".mp3");
-    // Playing the sound file
-    buttonAudio.play();
+    animatePress(randomChosenColour);
+    playSound(randomChosenColour);
 
 }
 
@@ -61,10 +64,50 @@ function nextSequence() {
 // the game pattern, and if so triggering the nextSequense function when the user
 // has finished the entire sequence
 function checkAnswer(currentLevel) {
+
+    // Checking if the last user clicked button is the same as the last in the gamepattern
     if (userClickedPattern[currentLevel] == gamePattern[currentLevel]) {
+        console.log("success")
+            //Checking if the length of the userClickedPattern array is the same as the length of the gamePattern array
         if (gamePattern.length == userClickedPattern.length) {
             setTimeout(function() { nextSequence() }, 1000);
 
         }
+    } else {
+        console.log("fail");
+        playSound("wrong");
+        document.body.classList.add("game-over");
+
+        document.getElementById("level-title").innerHTML = "Game over, press a key to restart";
+
+        setTimeout(function() {
+            document.body.classList.remove("game-over");
+        }, 2000);
+
+        startOver();
     }
+
+
+}
+
+function startOver() {
+    gameStarted = false;
+    userClickedPattern = [];
+    gamePattern = [];
+    level = 0;
+}
+
+function animatePress(color) {
+    // Setting a flash animation to the button with the generated colour
+    $("#" + color).addClass("pressed");
+    setTimeOut(function() {
+        $("#" + color).removeClass("pressed");
+    }, 100);
+}
+
+function playSound(name) {
+    // Creating an Audio object with its url set to the generated color's sound
+    let buttonAudio = new Audio("sounds/" + name + ".mp3");
+    // Playing the sound file
+    buttonAudio.play();
 }
